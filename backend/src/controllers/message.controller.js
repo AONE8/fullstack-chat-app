328de42b8112f21346c307f438148772f51be55c
AONE8 <malyshko.dp@gmail.com>
2026-02-10 10:20:51 +0200

    complete app

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,72 @@
+import cloudinary from "../lib/cloudinary.js";
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
+
+export async function getUsersForSidebar(req, res) {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.error("Error in getUsersForSidebar\n", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function getMessages(req, res) {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error("Error in getMessages\n", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function sendMessage(req, res) {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+
+    if (image) {
+      const uploadResp = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResp.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
+    res.status(200).json(newMessage);
+  } catch (error) {
+    console.error("Error in sendMessage\n", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
